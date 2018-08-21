435ca1dfa89a1f363422ca66d8a5534871c8e928
hugo valois <hvgaming@yahoo.com>
2018-08-21 01:11:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,21 +19,21 @@ def position_taken?(board,index)
   board[index] = token
 end
   
- def input_to_index(user_input)
+ 
+
+def input_to_index(user_input)
   converted_user_input = user_input.to_i - 1
 end
-
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
   
   if valid_move?(board, index)
-    move(board,index, token = "X")
+    move(board,index,token = "X")
     display_board(board)
     
  else
    turn(board)
   end
  end
-
